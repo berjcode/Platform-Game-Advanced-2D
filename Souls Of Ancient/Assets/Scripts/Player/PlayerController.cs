30a4710ca79100de729f5653cc0876a8c14e520c
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-09-02 15:27:19 +0300

    New Game

---
+++ Souls Of Ancient/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,115 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Animator animator;
+     private float vertical;
+     private float horizontal;
+    public float speedPlayer;
+    public float jumpForce;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    private SpriteRenderer _renderer;
+    void Start()
+    {
+        rb= GetComponent<Rigidbody2D>();
+        animator= GetComponent<Animator>();
+        _renderer =GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoving();
+        FlipMove();
+        Jump();
+    }
+
+
+    private void PlayerMoving()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        WalkAnimation();
+
+        transform.position += new Vector3(horizontal,0,0)*Time.deltaTime*speedPlayer;
+    }
+
+    #region  Walk Animation 
+   
+
+      private void WalkAnimation()
+    {
+        if(horizontal!=0)
+        {
+            animator.SetBool("isWalk",true);
+        }else
+        {
+            animator.SetBool("isWalk",false);
+        }
+    }
+    #endregion
+
+    #region  Jump
+     private void Jump()
+     {
+        if(Input.GetKeyDown("space"))
+        {
+            if(isGrounded==true)
+            {   
+                isGrounded=false;
+                rb.AddForce(new Vector2(0,jumpForce),ForceMode2D.Impulse);
+                JumpAnimation();
+            }
+            
+        }
+     }
+    #endregion
+
+    #region  JumpAnimation
+    void JumpAnimation()
+    {
+        if(horizontal!=0)
+             {
+                animator.SetBool("isJump",true);
+            }
+        else
+            {
+            animator.SetBool("isJump",false);
+            }
+    }
+   
+    #endregion
+    #region Character Flip
+
+    void FlipMove()
+    {   
+        if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            _renderer.flipX=false;
+        }
+        else if (Input.GetAxisRaw("Horizontal")<0)
+
+        {
+            _renderer.flipX = true;
+        }
+
+    }
+    #endregion
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag=="platform")
+        {
+            isGrounded =true;
+        }else
+        {
+            isGrounded =false;
+        }
+    }
+
+}
