f47148cfad170f11f62dab77f8f6fa77ecd6ec57
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-09-03 12:55:53 +0300

    Update

---
+++ Souls Of Ancient/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    private Transform playerPos;
+
+    private int health=5;
+    void Start()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,playerPos.position,speed*Time.deltaTime);
+        Killer();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+
+    {
+        if(col.tag =="Bullet")
+        {
+            health--;
+        }
+    }
+
+
+    public void Killer()
+    {
+        if(health ==0)
+        {
+            Destroy(gameObject);
+
+        }
+    }
+}
